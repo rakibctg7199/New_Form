3fcbc9e306e6549e14456e464aa9d8ca3357262a
somsomta <mdrakib1to9@gmail.com>
2025-04-07 05:15:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function copyBkash() {
+  navigator.clipboard.writeText("01979448661");
+  alert("বিকাশ নাম্বার কপি হয়েছে!");
+}
+
+document.getElementById("depositForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  window.location.href = "https://rakibctg7199.github.io/Loan-Form";
+});
